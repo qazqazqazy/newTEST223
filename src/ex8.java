69bf3a0781a839321a606468dfb7701808d4d7ad
qazqazqazy <oxygen2308@gmail.com>
2019-07-25 01:14:39 +0300

    Задача 8

---
+++ src/ex8.java
@@ -0,0 +1,15 @@
+public class ex8 {
+    public static void main(String args[])
+    {
+        int x = 1;
+        while( x < 51 )
+        {
+            System.out.print( + x );
+            x++;
+            System.out.print("\n");
+
+        }
+
+    }
+
+}
