a83c7c3ff82009a63b8a11b897f0c2ff3d747265
qazqazqazy <oxygen2308@gmail.com>
2019-07-30 02:25:54 +0300

    Задача 11

---
+++ src/ex11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ex11 {
+
+        public static void main(String args[]) {
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите  строку:");
+            String str = scan.nextLine();
+            System.out.println(str);
+
+        }
+    }
+
