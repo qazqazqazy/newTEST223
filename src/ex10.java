bcf5b4648b5f70968eb23edd99d8b8224e663618
qazqazqazy <oxygen2308@gmail.com>
2019-07-29 23:20:34 +0300

    Задача 10 (исправленная)

--- src/ex10.java
+++ src/ex10.java
@@ -4,18 +4,25 @@ public class ex10 {
 
     public static void main(String[] args)
     {
-        int tablе[][] = new int[4][4];
-        int i, j;
-        for (i=0; i<4; ++i)
-        {  for (j=0; j<4; ++j)
-        {System.out.println("Введите значения элементов массива: элемент "  + i + j);
-            Scanner scan = new Scanner(System.in);
-            int  number = scan.nextInt();
-            tablе[i][j] = number;
-            System.out.println("Элемент [" + i + j +"] : " + tablе[i][j]);}}
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите  размерность матрицы (шаг 1):");
+        int a = scan.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите  размерность матрицы (шаг 2):");
+        int b = sc.nextInt();
 
-        for (i=0; i<4; i++) {
-            for (j = 0; j < 1; ++j) {
+        int tablе[][] = new int[a][b];
+        int i, j;
+        for (i = 0; i < a; ++i)
+            {for (j = 0; j < b; ++j)
+                {System.out.println("Введите значения элементов массива: элемент "  + i + j);
+                Scanner n = new Scanner(System.in);
+                int  number = n.nextInt();
+                tablе[i][j] = number;
+                System.out.println("Элемент [" + i + j +"] : " + tablе[i][j]);}}
+
+        for (i = 0; i < 1; i++) {
+            for (j = 0; j < b; ++j) {
                 tablе[i][j] = tablе[i][j] * 3;
                 System.out.println("Новое значение элемента [" + i + j + "]  массива tablе: " + tablе[i][j]);
             }
