6af58702267c6564a90b31398a7e7cc8d8379a07
qazqazqazy <oxygen2308@gmail.com>
2019-07-29 22:07:37 +0300

    Задача 9 (исправленная)

--- src/ex9.java
+++ src/ex9.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
 public class ex9 {
-    public static void main(String[] args)
-
-    {
-       int myArray [] = new int[10];
-       int i;
-       for (i=0; i<10; i++)
-       {  System.out.println("Введите значения следующего элемента массива:");
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int  number = scan.nextInt();
-        myArray[i]=number;
-           System.out.println("Элемент [" + i +"] : " + myArray[i]);}
+        System.out.println("Введите размерность массива:");
+        int j = scan.nextInt();
+        int myArray[] = new int[j];
+        {int   i;
+            for (i = 0; i < j; i++) {
+                System.out.println("Введите значения следующего элемента массива:");
+                Scanner sc = new Scanner(System.in);
+                int number = sc.nextInt();
+                myArray[i] = number;
+                System.out.println("Элемент [" + i + "] : " + myArray[i]);
+            }
 
-        for (i=0; i<10; i++)
-        {myArray[i]=myArray[i]*2;
-        System.out.println("Новое значение элемента [" + i +"] массива myArray "+ myArray[i]); }
+            for (i = 0; i < j; i++) {
+                myArray[i] = myArray[i] * 2;
+                System.out.println("Новое значение элемента [" + i + "] массива myArray " + myArray[i]);
+            }
 
+        }
     }
-
 }
