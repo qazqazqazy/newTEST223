577ea39e2fb377a8fe927d68c309f30f03db0389
qazqazqazy <oxygen2308@gmail.com>
2019-07-31 03:36:01 +0300

    Задание 13 (Исправленное)

--- src/ex13.java
+++ src/ex13.java
@@ -9,16 +9,16 @@ public class ex13 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите первую строку для сравнения");
         String str1 = scan.nextLine();
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         System.out.println("Введите вторую строку для сравнения");
-        String str2 = sc.nextLine();
+        String str2 = scan.nextLine();
         int len1 = str1.length();
         int len2 = str2.length();
 
         if (len1 > len2) {
             System.out.println(str1);
         }
-        if (len1 < len2) {
+        else if (len1 < len2) {
             System.out.println(str2); }
         else {
             System.out.println("Длины строк равны");
