19ef9e271f0b1093ff4f08bc7a0312cbb5b9dedd
qazqazqazy <oxygen2308@gmail.com>
2019-08-15 15:01:13 +0300

    Итоговое задание 1 (исправление2)

--- src/itog11.java
+++ src/itog11.java
@@ -8,16 +8,16 @@ public class itog11 {
         System.out.println("Введите число в бинарном формате:");
         String a = scan.nextLine();
         char[] ms = a.toCharArray();
-
+/*
         int[] msint = new int[ms.length];
         for (int i = 0; i < ms.length; i ++) {
             msint[i] =  Integer.parseInt(String.valueOf(ms[i]));
             }
-
+*/
         int tmp = 0;
-        for (int i = 0; i < msint.length; i ++) {
-            if (msint [i] != 0) {
-            tmp = tmp + (int) Math.pow(2, (msint.length - 1 - i)) * msint[i];
+        for (int i = 0; i < ms.length; i ++) {
+            if (Integer.parseInt(String.valueOf(ms[i])) != 0) {
+            tmp = tmp + (int) Math.pow(2, (ms.length - 1 - i));
             }
         }
         System.out.println("Результат в десятичной системе счисления: " + tmp);
